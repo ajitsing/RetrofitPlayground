7197f12a1ffd5507fa1e16776a2fa1cf6c83a578
AjitSingh <jeetsingh.ajit@gmail.com>
2017-06-21 14:23:50 +0530

    Adding cache of 10 seconds to repos call

--- app/src/main/java/com/singhajit/retrofitplayground/SingleRequestActivity.java
+++ app/src/main/java/com/singhajit/retrofitplayground/SingleRequestActivity.java
@@ -2,12 +2,15 @@ package com.singhajit.retrofitplayground;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.singhajit.retrofitplayground.github.GithubApiClient;
 import com.singhajit.retrofitplayground.github.Repository;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
@@ -15,24 +18,47 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SingleRequestActivity extends AppCompatActivity {
+
+  private GithubApiClient githubApiClient;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    Cache cache = new Cache(new File(this.getCacheDir(), "http"), 10 * 1024 * 1024);
+
+    HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+    interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+    OkHttpClient client = new OkHttpClient.Builder()
+        .cache(cache)
+        .addInterceptor(interceptor)
+        .build();
+
     Retrofit githubRetrofit = new Retrofit.Builder()
+        .client(client)
         .baseUrl("https://api.github.com/")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .build();
 
-    GithubApiClient githubApiClient = githubRetrofit.create(GithubApiClient.class);
+    githubApiClient = githubRetrofit.create(GithubApiClient.class);
+    makeRequest();
+  }
 
+  public void fetchData(View view) {
+    makeRequest();
+  }
+
+  private void makeRequest() {
+    displayRepos(new ArrayList<Repository>());
     Observable<List<Repository>> ajitsingRepos = githubApiClient.repos("ajitsing");
     ajitsingRepos.subscribeOn(Schedulers.newThread())
         .observeOn(AndroidSchedulers.mainThread())
@@ -54,7 +80,6 @@ public class SingleRequestActivity extends AppCompatActivity {
           public void onComplete() {
           }
         });
-
   }
 
   private void displayRepos(List<Repository> repos) {
--- app/src/main/java/com/singhajit/retrofitplayground/github/GithubApiClient.java
+++ app/src/main/java/com/singhajit/retrofitplayground/github/GithubApiClient.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface GithubApiClient {
   @GET("users/{user}/repos")
+  @Headers("Cache-Control: max-age=10")
   Observable<List<Repository>> repos(@Path("user") String user);
 }
